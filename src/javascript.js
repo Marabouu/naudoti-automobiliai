4e4efcecf4ce6d1ebd6b86b87d9398c7da4325ed
Rolandas Skrebutenas <rolandasskrebutenas@Rolandass-MBP.lan>
2022-08-23 20:08:20 +0300

    updatev4

--- src/javascript.js
+++ src/javascript.js
@@ -5,8 +5,8 @@ const arrayFirstRemove = document.getElementById("btn3");
 const arrayLastRemove = document.getElementById("btn4");
 const arrayReverse = document.getElementById("btn5");
 const inputt = document.getElementById("name");
-
 function stringToP() {
+  result.innerHTML = "";
   namesList.forEach((name) => {
     const nameParagraph = document.createElement("p");
     nameParagraph.textContent = name;
@@ -14,33 +14,23 @@ function stringToP() {
     inputt.value = "";
   });
 }
-
 arrayPush.addEventListener("click", function () {
-  result.innerHTML = "";
   namesList.push(inputt.value);
   stringToP();
 });
-
 arrayUnshift.addEventListener("click", function () {
-  result.innerHTML = "";
   namesList.unshift(inputt.value);
   stringToP();
 });
-
 arrayFirstRemove.addEventListener("click", function () {
-  result.innerHTML = "";
   namesList.shift(inputt.value);
   stringToP();
 });
-
 arrayLastRemove.addEventListener("click", function () {
-  result.innerHTML = "";
   namesList.pop(inputt.value);
   stringToP();
 });
-
 arrayReverse.addEventListener("click", function () {
-  result.innerHTML = "";
   namesList.reverse(inputt.value);
   stringToP();
 });
